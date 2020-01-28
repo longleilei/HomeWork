94824f745ae3ae3ca5a0f7a0c326b01d1e336e51
yuriyjava <yp0620@gmail.com>
2020-01-28 19:26:58 +0200

    hw-12-fix

--- hw12/js/home_work.js
+++ hw12/js/home_work.js
@@ -20,26 +20,35 @@ class HttpReq{
 
 let customHttp = new HttpReq(); 
 customHttp.get('https://jsonplaceholder.typicode.com/users', (response)=>{
-for (let i = 0; i<response.length; i++){
-  console.log(response[i].name);
-  let fragment = "<div class='card'><div class='card-header' id='headingOne'><h2 class='mb-0'><button class='btn btn-link' type='button' data-toggle='collapse' data-target='#collapseOne' aria-expanded='true' aria-controls='collapseOne'>Collapsible Group Item #1</button</h2></div><div id='collapseOne'class='collapse show' aria-labelledby='headingOne' data-parent='#accordionExample'><div class='card-body'>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.</div></div></div>"
-
-  document.getElementsByClassName('accordion')[0].insertAdjacentHTML('beforeend',fragment);
-
-  document.getElementsByTagName('button')[i].innerHTML = response[i].name; 
-
-
-  document.getElementsByClassName('card-body')[i].innerHTML = `The user's id is ${response[i].id}, username is ${response[i].username} and email is ${response[i].email}`; 
+  let container = document.getElementsByClassName('accordion')[0];
+for (let i = 0; i < response.length; i++){
+    let user = response[i];
+    console.log(user.name);
+    let fragment = generateTemplate(user);
+    container.insertAdjacentHTML('beforeend',fragment);
   }
 
-// if button 'area expanded' == true, the rest of areas are false; 
-
-//let child = document.getElementsByClassName('card'); 
-//child.parentNode.removeChild(child); 
-
-//document.getElementsByClassName('card').innerHTML = ' '; 
 })
 
+function generateTemplate(user) {
+  return /* html */`
+  <div class="card">
+  <div class="card-header" id="card-item-${user.id}">
+    <h2 class="mb-0">
+      <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse-${user.id}" aria-controls="collapse-${user.id}" aria-expanded="false">
+        ${user.name}
+      </button>
+    </h2>
+  </div>
+
+  <div id="collapse-${user.id}" class="collapse" aria-labelledby="card-item-${user.id}" data-parent="#accordionExample" >
+    <div class="card-body">
+      The user's id is ${user.id}, username is ${user.username} and email is ${user.email}
+    </div>
+  </div>
+</div>`
+  
+}
 
 
 
